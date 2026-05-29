343aac118d1ac135c18e35d1eecd62fc2a34fe30
Test830260 <aindiaarchitecture@gmail.com>
2026-05-29 23:18:41 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,10 @@
-import "./style.css";
-import GameBoard from "./GameBoard";
+import "./games/sudoku/style.css";
+import GameBoard from "./games/sudoku/GameBoard";
 
 export default function App() {
   return (
     <div className="app">
       <div className="title">🧩 Sudoku</div>
-
       <GameBoard />
     </div>
   );
