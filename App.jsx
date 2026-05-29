8c18c2a6a16f2409dd99fcf38c03a1fcc0ceeb42
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 01:20:57 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,126 @@
+import { useState } from "react";
+
 import "./games/sudoku/style.css";
+
 import GameBoard from "./games/sudoku/GameBoard";
 
 export default function App() {
+  const [page, setPage] =
+    useState("home");
+
   return (
     <div className="app">
-      <div className="title">🧩 Sudoku</div>
-      <GameBoard />
+
+      {page === "home" && (
+        <>
+          <div className="title">
+            🎮 Bolish Games
+          </div>
+
+          <button
+            className="newgame"
+            onClick={() =>
+              setPage("games")
+            }
+          >
+            🎲 O'yinlar
+          </button>
+
+          <button
+            className="stats-btn"
+            onClick={() =>
+              setPage("rating")
+            }
+          >
+            🏆 Reyting
+          </button>
+        </>
+      )}
+
+      {page === "games" && (
+        <>
+          <div className="title">
+            🎲 O'yinlar
+          </div>
+
+          <button
+            className="newgame"
+            onClick={() =>
+              setPage("sudoku")
+            }
+          >
+            🧩 Sudoku
+          </button>
+
+          <button
+            className="stats-btn"
+            onClick={() =>
+              setPage("home")
+            }
+          >
+            ⬅ Orqaga
+          </button>
+        </>
+      )}
+
+      {page === "rating" && (
+        <>
+          <div className="title">
+            🏆 TOP 100
+          </div>
+
+          <div className="top-card">
+            Reyting tizimi
+            keyin qo'shiladi
+          </div>
+
+          <button
+            className="stats-btn"
+            onClick={() =>
+              setPage("home")
+            }
+          >
+            ⬅ Orqaga
+          </button>
+        </>
+      )}
+
+      {page === "sudoku" && (
+        <>
+          <div className="title">
+            🧩 Sudoku
+          </div>
+
+          <button
+            className="newgame"
+            onClick={() =>
+              setPage("play")
+            }
+          >
+            ▶ O'YNASH
+          </button>
+
+          <button
+            className="stats-btn"
+            onClick={() =>
+              setPage("games")
+            }
+          >
+            ⬅ Orqaga
+          </button>
+        </>
+      )}
+
+      {page === "play" && (
+        <>
+          <div className="title">
+            🧩 Sudoku
+          </div>
+
+          <GameBoard />
+        </>
+      )}
+
     </div>
   );
-}
+      }
