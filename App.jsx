9fd8cb03fda8be62bfbcd3c050ce1f5571771ca2
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 01:32:03 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,83 @@
+import { useState } from "react";
+
 export default function App() {
-  return (
-    <h1
-      style={{
-        color: "red",
-        fontSize: "60px"
-      }}
-    >
-      TEST 12345
-    </h1>
-  );
+  const [page, setPage] =
+    useState("home");
+
+  if (page === "home") {
+    return (
+      <div className="app">
+        <h1 className="title">
+          🎮 Bolish Games
+        </h1>
+
+        <button
+          className="newgame"
+          onClick={() =>
+            setPage("games")
+          }
+        >
+          🎲 O'yinlar
+        </button>
+
+        <button
+          className="stats-btn"
+          onClick={() =>
+            setPage("rating")
+          }
+        >
+          🏆 Reyting
+        </button>
+      </div>
+    );
+  }
+
+  if (page === "games") {
+    return (
+      <div className="app">
+        <h1 className="title">
+          🎲 O'yinlar
+        </h1>
+
+        <button
+          className="newgame"
+          onClick={() =>
+            setPage("sudoku")
+          }
+        >
+          🧩 Sudoku
+        </button>
+
+        <button
+          className="stats-btn"
+          onClick={() =>
+            setPage("home")
+          }
+        >
+          ⬅ Orqaga
+        </button>
+      </div>
+    );
+  }
+
+  if (page === "rating") {
+    return (
+      <div className="app">
+        <h1 className="title">
+          🏆 Reyting
+        </h1>
+
+        <button
+          className="stats-btn"
+          onClick={() =>
+            setPage("home")
+          }
+        >
+          ⬅ Orqaga
+        </button>
+      </div>
+    );
+  }
+
+  return null;
 }
