2ec7b41d688b5fb7d21468a78f755f7ec93efff4
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 02:41:19 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,68 +1,12 @@
-import { useState } from "react";
-
-import "./src/games/sudoku/style.css";
-
-import HomePage from "./src/pages/HomePage";
-import GamesPage from "./src/pages/GamesPage";
-import RatingPage from "./src/pages/RatingPage";
-import SudokuPage from "./src/pages/SudokuPage";
-
-import GameBoard from "./src/games/sudoku/GameBoard";
-
 export default function App() {
-  const [page, setPage] = useState("home");
-
   return (
-    <div className="app">
-
-      {page === "home" && (
-        <HomePage setPage={setPage} />
-      )}
-
-      {page === "games" && (
-        <GamesPage setPage={setPage} />
-      )}
-
-      {page === "rating" && (
-        <RatingPage setPage={setPage} />
-      )}
-
-      {page === "sudoku" && (
-        <SudokuPage setPage={setPage} />
-      )}
-
-      {page === "play" && (
-        <>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginBottom: "15px"
-            }}
-          >
-            <button
-              className="stats-btn"
-              onClick={() => setPage("sudoku")}
-            >
-              ⬅ Orqaga
-            </button>
-
-            <div
-              style={{
-                color: "#38bdf8",
-                fontSize: "28px",
-                fontWeight: "700"
-              }}
-            >
-              🧩 Sudoku
-            </div>
-          </div>
-
-          <GameBoard />
-        </>
-      )}
-
-    </div>
+    <h1
+      style={{
+        color: "lime",
+        fontSize: "100px"
+      }}
+    >
+      APP TEST
+    </h1>
   );
 }
