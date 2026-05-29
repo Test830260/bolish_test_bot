606409f9b7f722b315fb357a8791b745459498fb
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 02:19:29 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(
+  document.getElementById('root')
+).render(
+  <h1
+    style={{
+      color: 'red',
+      fontSize: '80px'
+    }}
+  >
+    MAIN TEST
+  </h1>
+)
