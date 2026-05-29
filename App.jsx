13d92dd0a4f5383bf8b452d3488270d2a6e52d8f
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 02:11:44 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import "./games/sudoku/style.css";
-
 import HomePage from "./pages/HomePage";
 import GamesPage from "./pages/GamesPage";
 import RatingPage from "./pages/RatingPage";
@@ -10,77 +8,72 @@ import SudokuPage from "./pages/SudokuPage";
 import GameBoard from "./games/sudoku/GameBoard";
 
 export default function App() {
-const [page, setPage] =
-useState("home");
+  const [page, setPage] =
+    useState("home");
 
-return (
-<div className="app">
+  return (
+    <div className="app">
 
-  {page === "home" && (
-    <HomePage
-      setPage={setPage}
-    />
-  )}
+      {page === "home" && (
+        <HomePage
+          setPage={setPage}
+        />
+      )}
 
-  {page === "games" && (
-    <GamesPage
-      setPage={setPage}
-    />
-  )}
+      {page === "games" && (
+        <GamesPage
+          setPage={setPage}
+        />
+      )}
 
-  {page === "rating" && (
-    <RatingPage
-      setPage={setPage}
-    />
-  )}
+      {page === "rating" && (
+        <RatingPage
+          setPage={setPage}
+        />
+      )}
 
-  {page === "sudoku" && (
-    <SudokuPage
-      setPage={setPage}
-    />
-  )}
+      {page === "sudoku" && (
+        <SudokuPage
+          setPage={setPage}
+        />
+      )}
 
-  {page === "play" && (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent:
-            "space-between",
-          alignItems:
-            "center",
-          marginBottom:
-            "15px"
-        }}
-      >
-        <button
-          className="stats-btn"
-          onClick={() =>
-            setPage(
-              "sudoku"
-            )
-          }
-        >
-          ⬅ Orqaga
-        </button>
+      {page === "play" && (
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent:
+                "space-between",
+              alignItems:
+                "center",
+              marginBottom: "15px"
+            }}
+          >
+            <button
+              className="stats-btn"
+              onClick={() =>
+                setPage("sudoku")
+              }
+            >
+              ⬅ Orqaga
+            </button>
 
-        <div
-          className="title"
-          style={{
-            margin: 0,
-            fontSize:
-              "28px"
-          }}
-        >
-          🧩 Sudoku
-        </div>
-      </div>
+            <div
+              style={{
+                color: "#38bdf8",
+                fontSize: "28px",
+                fontWeight: "700"
+              }}
+            >
+              🧩 Sudoku
+            </div>
+          </div>
 
-      <GameBoard />
-    </>
-  )}
+          <GameBoard />
+        </>
+      )}
 
-</div>
-
-);
+    </div>
+  );
 }
