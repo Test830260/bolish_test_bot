2ea01e48802d84e48598c1e102043235217366cc
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 14:12:41 +0500

    Update GameBoard.jsx

--- src/games/sudoku/GameBoard.jsx
+++ src/games/sudoku/GameBoard.jsx
@@ -7,7 +7,13 @@ export default function GameBoard({ goBack }) {
   return (
     <div style={{ minHeight: "100vh", background: "#08080e", color: "white" }}>
       <h1>GAMEBOARD TEST</h1>
+<NumberPad
+  enterNumber={() => {}}
+/>
 
+<h1 style={{color:"red"}}>
+  NUMBERPAD TEST
+</h1>
       <TopBar
         seconds={0}
         score={1200}
