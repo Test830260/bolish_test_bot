107a84569871e0bc8548a91e2421ba4398528fc9
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 13:40:44 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -37,7 +37,7 @@ export default function App() {
           >
             ← Orqaga
           </button>
-          <GameBoard />
+          <GameBoard goBack={() => navigate("sudoku")} />
         </div>
       )}
     </div>
