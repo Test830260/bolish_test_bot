2bd90b906049b791ce2cb3c9b548bc232af68e79
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 14:53:13 +0500

    Update NumberPad.jsx

--- src/games/sudoku/components/NumberPad.jsx
+++ src/games/sudoku/components/NumberPad.jsx
@@ -1,15 +1,44 @@
 export default function NumberPad({
   enterNumber
 }) {
+  const numbers = [
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
+  ];
+
   return (
-    <div className="numbers">
-      {[1,2,3,4,5,6,7,8,9].map((n) => (
+    <div
+      style={{
+        padding: "15px",
+        display: "grid",
+        gridTemplateColumns:
+          "repeat(3, 1fr)",
+        gap: "10px",
+        maxWidth: "350px",
+        margin: "20px auto"
+      }}
+    >
+      {numbers.map((num) => (
         <button
-          key={n}
-          className="num-btn"
-          onClick={() => enterNumber(n)}
+          key={num}
+          onClick={() =>
+            enterNumber(num)
+          }
+          style={{
+            height: "65px",
+            borderRadius: "16px",
+            border:
+              "1px solid #38bdf8",
+            background:
+              "#18223d",
+            color: "#ffffff",
+            fontSize: "26px",
+            fontWeight: "700",
+            cursor: "pointer"
+          }}
         >
-          {n}
+          {num}
         </button>
       ))}
     </div>
