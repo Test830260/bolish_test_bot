f03bf4a1ca65b4b7cbe5a58af982efa889255c97
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 09:33:44 +0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,45 +1,10 @@
-import "../src/styles/home.css";
-
-export default function HomePage({ setPage }) {
+export default function HomePage() {
   return (
-    <div className="home-page">
-
-      <div className="hero-card">
-        <div className="logo">🎮</div>
-
-        <h1>Bolish Games</h1>
-
-        <p>
-          Premium mini o‘yinlar platformasi
-        </p>
-
-        <button
-          className="play-btn"
-          onClick={() => setPage("games")}
-        >
-          O‘yinlarni ochish →
-        </button>
-      </div>
-
-      <div className="stats-grid">
-
-        <div className="stat-card">
-          <h2>1</h2>
-          <span>O‘yin</span>
-        </div>
-
-        <div className="stat-card">
-          <h2>0</h2>
-          <span>Rekord</span>
-        </div>
-
-        <div className="stat-card">
-          <h2>∞</h2>
-          <span>Imkoniyat</span>
-        </div>
-
-      </div>
-
-    </div>
+    <h1 style={{
+      color: "lime",
+      fontSize: "70px"
+    }}>
+      YANGI HOME
+    </h1>
   );
 }
