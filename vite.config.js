36daf8c34c028347c4a2ad9638cbb1612c7cc823
Test830260 <aindiaarchitecture@gmail.com>
2026-05-30 12:40:23 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",
+  build: {
+    outDir: "dist",
+  },
   preview: {
-    allowedHosts: ["bolishtestbot-production.up.railway.app"]
-  }
+    allowedHosts: ["bolishtestbot-production.up.railway.app"],
+    port: process.env.PORT || 4173,
+    host: "0.0.0.0",
+  },
 });
